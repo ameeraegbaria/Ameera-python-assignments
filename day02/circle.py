1962a9b6821a4741858ad0e720ced96870f3834c
Ameera Egbaria <ameera.egbaria@weizmann.ac.il>
2024-11-12 17:35:08 +0200

    Add files via upload

---
+++ day02/circle.py
@@ -0,0 +1,16 @@
+# circle.py
+
+import math
+
+# Define the radius of the circle
+radius = 7  
+
+# Calculate the area
+area = math.pi * radius ** 2
+
+# Calculate the circumference
+circumference = 2 * math.pi * radius
+
+# Print out the results
+print("The area of the circle is:", area)
+print("The circumference of the circle is:", circumference)
---
+++ day02/rectangle.py
@@ -0,0 +1,15 @@
+# rectangle.py
+
+# Define the width and length of the rectangle
+width = 10  
+length = 20  
+
+# Calculate the area
+area = width * length
+
+# Calculate the circumference
+circumference = 2 * (width + length)
+
+# Print out the results
+print("The area of the rectangle is:", area)
+print("The circumference of the rectangle is:", circumference)
